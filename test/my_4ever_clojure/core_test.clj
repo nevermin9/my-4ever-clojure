258ceb0fc8477f23723ed037ff3b09304823ce36
nevermin9 <chizzza@gmail.com>
2022-12-26 20:49:51 +0100

    setup project

---
+++ test/my_4ever_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns my-4ever-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [my-4ever-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
