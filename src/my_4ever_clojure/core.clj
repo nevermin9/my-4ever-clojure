d23c117f545331ccdab45686872ae1ae051b4337
nevermin9 <chizzza@gmail.com>
2023-01-05 18:02:54 +0100

    explore specification

--- src/my_4ever_clojure/core.clj
+++ src/my_4ever_clojure/core.clj
@@ -173,7 +173,7 @@
   (last coll))
 
 (comment
-  (run-answer get-last-el 
+  (run-answer get-last-el
               (fn [a] (= (a [1 2 3 4 5]) 5))
               (fn [a] (= (a '(10 20 30)) 30)))
   :rcf)
@@ -184,7 +184,7 @@
   (first (take-last 2 coll)))
 
 (comment
-  (run-answer penultimate-el 
+  (run-answer penultimate-el
               (fn [a] (= (a (list 1 2 3 4 5)) 4))
               (fn [a] (= (a [:a :b :c :d]) :c))
               (fn [a] (= (a [[1 2] [3 4]]) [1 2])))
@@ -237,12 +237,12 @@
 ; #24 Sum it all up
 (comment
   (run-answer
-    (fn [coll] (reduce + coll))
-    #(= (% [1 2 3]) 6)
-    #(= (% (list 0 -2 5 5)) 8)
-    #(= (% #{4 2 1}) 7)
-    #(= (% '(0 0 -1)) -1)
-    #(= (% '(1 10 3)) 14))
+   (fn [coll] (reduce + coll))
+   #(= (% [1 2 3]) 6)
+   #(= (% (list 0 -2 5 5)) 8)
+   #(= (% #{4 2 1}) 7)
+   #(= (% '(0 0 -1)) -1)
+   #(= (% '(1 10 3)) 14))
   :rcf)
 
 ; #25 Find the odd number
@@ -250,12 +250,12 @@
 
 (comment
   (run-answer
-    #(filter odd? %)
-    #(= (% #{1 2 3 4 5}) '(1 3 5))
-    #(= (% [4 2 1 6]) '(1))
-    #(= (% [2 2 4 6]) '())
-    #(= (% [1 1 1 3]) '(1 1 1 3)))
-:rcf)
+   #(filter odd? %)
+   #(= (% #{1 2 3 4 5}) '(1 3 5))
+   #(= (% [4 2 1 6]) '(1))
+   #(= (% [2 2 4 6]) '())
+   #(= (% [1 1 1 3]) '(1 1 1 3)))
+  :rcf)
 
 ; #26 Fibonacci
 
@@ -275,7 +275,7 @@
   (fibonacci 3)
   (fibonacci 6)
   (fibonacci 8)
-:rcf)
+  :rcf)
 
 ; #27 Palindrome Detector
 (defn palindrom-detector [item]
@@ -287,7 +287,158 @@
   (palindrom-detector [:foo :bar :foo])
   (palindrom-detector '(1 1 3 3 1 1))
   (palindrom-detector '(:a :b :c))
-:rcf)
+  :rcf)
+
+; #28 Flatten a Sequence
+; coll? concat (type instance?)
+;; (defn flatten-a-seq [seq]
+;;   (loop [result []
+;;          curr (first seq)
+;;          other (rest seq)]
+;;     (if )))
+
+; ------ (as->) !
+; ------ (cond->)
+; ------ (cond->>)
+(def users [{:name "James"
+             :age 27
+             :address {:city "Kyiv"
+                       :index "02156"}}
+            {:name "Mira"
+             :age 68
+             :address {:city "Lviv"
+                       :index "05678"}}])
+
+(defn get-city [user-coll]
+  (as-> user-coll $
+    (nth $ 1)
+    (:address $)
+    (:city $)))
+
+(defn check-cond [user]
+  (cond-> []
+    (contains? user :name) (conj (:name user))
+    (contains? user :age) (conj (:age user))
+    (contains? user :profession) (conj (:profession user))
+    (contains? user :address) (conj
+                               (as-> user $
+                                 (:address $)
+                                 (:city $)))))
+
+(defn do-staff [coll {:keys [map-fn reduce-fn]}]
+  (cond->> coll
+    map-fn (map map-fn)
+    reduce-fn (reduce reduce-fn)))
+
+(defn guess-the-int [i]
+  (condp = i
+    1 "one"
+    2 "two"
+    3 "three"
+    "cannot regonize"))
+
+(defn guess-coll [col]
+  (condp some col
+    #{1 2 3} :>> println
+    #{4 5 6} :>> #(str % " is answer")
+    #{0 9 8} :>> dec))
+
+(defn explore-for []
+  (for [x (take 10 (range 43 88))
+        :let [y (* x 2)]
+        :when (even? x)]
+    [x y]))
+
+(defn explore-if-let [nums]
+  (if-let [n (seq (filter even? nums))]
+    (apply println n)
+    (println "Goodbye")))
 
 
+(comment
+  (explore-if-let [1 2 3 4 56 7])
+  (explore-if-let [1 3 7])
+  (get-city users)
+  (check-cond (first users))
+  (explore-for)
+
+  (do-staff [1 2 3] {:reduce-fn #(+ %1 %2)})
+  (guess-the-int 5)
+  (guess-coll [9 0])
+
+  (drop-while (fn [i] (< i 5)) '(1 2 3 4 5 6 7 8 9))
+
+  (ffirst '([1 2] [3 4] [5 6]))
+  (fnext '([1 2] [3 4] [5 6]))
+
+  (filterv #(< % 5) '(1 2 3 4 5 6 7 8 9))
+
+  (find {:a "a" :b "b" :c "2"} :a)
+
+  (flatten (seq {:a "a" :b "b"}))
+
+  (let [a [{:a "hi"} {:b "hey"}]
+        b [{:c "yo"} {:d "hiya"}]
+        c {:e ["hola" "bonjour"]}]
+    ((comp vec flatten vector) a b c))
+
+  (group-by odd? '(1 2 3 4 5 67 8 9))
+
+  (interleave [:name :age :prof] ["Anton" 27 "Dev"])
+  (apply str (interpose ", " ["Anton" "Oleg" "Nazar"]))
+  ((juxt count first) ["anton" "oleg" "nazr"])
+
+;;   (update-in)
+;;   (assoc-in)
+
+;;   (comp)
+;;   (partial)
+;;   (memoize)
+
+;;   (cycle [2])
+;;   (repeat 10 2)
+;;   (range 0 20)
+;;   (take 10 (iterate (partial + 2) 10))
+  (partition 4 2 (range 20))
+  (partition 4 2 '(:a :b :c :d :e :f :g :h :i :j :k :l :m :n :o))
+  (partition 4 2 '(:z) '(:a :b :c :d :e :f :g :h :i :j :k :l :m :n :o))
+
+  (partition-by identity "lllleeeeerrrrrooooyyy")
+
+  (keep even? (range 22 33))
+  (keep-indexed #(when (odd? %1) %2) '(:a :b :c :d :e :f))
+  (map key {:a 2})
+
+  (map-indexed (fn [& what]
+                 (apply println what)) [:a :b :c :d])
+
+  (mapcat (fn [arr]
+            (distinct arr))
+          [[1 22 22 3 57 1 1] [2 2 3 3 5 6 10 43 1]])
+
+  (mapv inc [77 88 99 101])
+
+  (max-key count "anton" "igor" "dima" "platon")
+
+  (merge {:id "id" :name "name"} {:age 28 :size "l" :id "lol"})
+
+;;   (merge-with)
+
+;;   (into {"lisp" ["a" "b"]} {"lisp" ["c"]})
+
+  (nth [] 2 "nothing found")
+
+  (nthnext [:a :b :c :d :e :f] 2)
+
+  (nthrest [:a :b :c :d :e :f] 2)
+
+  ; peek - as 'last for vec (more efficient)
+  ; peek - as 'first for list
+  (time (last (vec (range 10001))))
+  (time (peek (vec (range 10001))))
+
+  (pop '(1 2 3))
+  (pop [1 2 3])
+
+  :rcf)
 
